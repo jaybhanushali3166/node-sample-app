45468bb047c9c7f69865455a9a86d5e58d9cfb12
Jay Bhanushali <jay.bhanushali@idrive.com>
2023-06-12 17:09:43 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = 80;
+const config = require("./db.config");
+
+// parse application/json
+app.use(bodyParser.json());
+// Enable CORS
+app.use(cors());
+
+// APIs
+app.get("/", (req, res) => {
+    res.send('<h1>My Node test App</h1>')
+});
+
+app.listen(PORT, () => {
+  console.log("Server listening on:", PORT);
+});
